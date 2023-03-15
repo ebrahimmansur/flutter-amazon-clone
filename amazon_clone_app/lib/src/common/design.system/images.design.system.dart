cc9b75b2655128d7b7050a97732ff67ffa6932de
Ebrahim Mansur <ebrahimmansur90117@gmail.com>
2023-03-15 19:13:42 +0200

    ✅ add : images design system

---
+++ amazon_clone_app/lib/src/common/design.system/images.design.system.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+class ImagesDesignSystem {
+  final List<String> carouselImages;
+  const ImagesDesignSystem._({
+    required this.carouselImages,
+  });
+
+  factory ImagesDesignSystem.create() =>
+      const ImagesDesignSystem._(carouselImages: [
+        'https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/WLA/TS/D37847648_Accessories_savingdays_Jan22_Cat_PC_1500.jpg',
+        'https://images-eu.ssl-images-amazon.com/images/G/31/img2021/Vday/bwl/English.jpg',
+        'https://images-eu.ssl-images-amazon.com/images/G/31/img22/Wireless/AdvantagePrime/BAU/14thJan/D37196025_IN_WL_AdvantageJustforPrime_Jan_Mob_ingress-banner_1242x450.jpg',
+        'https://images-na.ssl-images-amazon.com/images/G/31/Symbol/2020/00NEW/1242_450Banners/PL31_copy._CB432483346_.jpg',
+        'https://images-na.ssl-images-amazon.com/images/G/31/img21/shoes/September/SSW/pc-header._CB641971330_.jpg',
+      ]);
+}
